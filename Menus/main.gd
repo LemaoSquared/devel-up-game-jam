4d14c4a0df5ca43e1f54a51b87391e1f03a7cbbc
Vincent Casuyac <vecasuyac01202401552@usep.edu.ph>
2026-08-02 00:06:52 +0800

    Update Transition(Polaroid)/GameOver

--- Menus/main.gd
+++ Menus/main.gd
@@ -1,14 +1,15 @@
 extends Node2D
+const GameOverScreen := preload("res://Menus/GameOverScreen.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$StartPanel.game_started.connect($Background.close_cinematic_bars)
 	$StartPanel.game_started.connect($ProgressBar.start_countdown)
 	$StartPanel.game_started.connect(PauseManager.enable_pause)
-	
+	$ProgressBar.countdown_finished.connect(_on_progress_bar_countdown_finished)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_progress_bar_countdown_finished() -> void:
+	await $Transition.transition()
+	$Background.retreat_cinematic_bars()
+	var game_over_instance := GameOverScreen.instantiate()
+	add_child(game_over_instance)
+	await $Transition.Return()
