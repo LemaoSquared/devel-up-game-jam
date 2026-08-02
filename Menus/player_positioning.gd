bcc2909e151f56414646fda51fe29e7a46cf01f3
Webster Miguelle Isidor <wmdisidor01202401034@usep.edu.ph>
2026-08-02 19:22:37 +0800

    animated_ending_cutscene implementation arn's animations added
    *Small nitpick, some color errors in the walking animation that should be remedied*

--- Menus/player_positioning.gd
+++ Menus/player_positioning.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends AnimatedSprite2D
 
 @onready var bg_manager: Node2D = $"../BackgroundManager"
 
@@ -12,18 +12,21 @@ func _on_background_changed(bg_index: int):
 	match bg_index:
 		0:
 			# Rules for Background 0 (e.g., The Menu)
-			self.scale = Vector2(1.687, 1.687)
-			self.position = Vector2(264.0, 520)
+			self.scale = Vector2(7.445, 7.445)
+			self.position = Vector2(448.0, 520.0)
+			self.play("outside_idle")
 			print("Sprite set up for Background 0")
 			
 		1:
 			# Rules for Background 1 (e.g., Level 1)
-			self.scale = Vector2(2.344, 2.344)
-			self.position = Vector2(264.0, 400.0)
+			self.scale = Vector2(2.338, 2.338)
+			self.position = Vector2(208, 408.325)
+			self.play("Inside_Walk")
 			print("Sprite set up for Background 1")
 			
 		2:
 			# Rules for Background 2 
-			self.scale = Vector2(0.672, 0.672)
-			self.position = Vector2(256.0, 512.0)
+			self.scale = Vector2(0.89, 0.89)
+			self.position = Vector2(296.0, 520.0)
+			self.play("outside_walk")
 			print("Sprite set up for Background 2")
